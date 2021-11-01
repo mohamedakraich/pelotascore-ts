b2c902229f34687bdba31c41a626e0229f3ef83e
AkraichLand <mohamed.akraich@gmail.com>
2021-11-01 09:53:30 +0100

    Adding the leagues file

---
+++ data/leagues.ts
@@ -0,0 +1,63 @@
+export const leagues = [
+  {
+    id: 'austria_1',
+    link: 'https://www.soccerstats.com/results.asp?league=austria&pmtype=bydate',
+  },
+  ,
+  {
+    id: 'belgium_1',
+    link: 'https://www.soccerstats.com/results.asp?league=belgium&pmtype=bydate',
+  },
+  {
+    id: 'denmark_1',
+    link: 'https://www.soccerstats.com/results.asp?league=denmark&pmtype=bydate',
+  },
+  {
+    id: 'england_1',
+    link: 'https://www.soccerstats.com/results.asp?league=england&pmtype=bydate',
+  },
+  {
+    id: 'france_1',
+    link: 'https://www.soccerstats.com/results.asp?league=france&pmtype=bydate',
+  },
+  {
+    id: 'germany_1',
+    link: 'https://www.soccerstats.com/results.asp?league=germany&pmtype=bydate',
+  },
+  {
+    id: 'italy_1',
+    link: 'https://www.soccerstats.com/results.asp?league=italy&pmtype=bydate',
+  },
+  {
+    id: 'morocco_1',
+    link: 'https://www.soccerstats.com/results.asp?league=morocco&pmtype=bydate',
+  },
+  {
+    id: 'netherlands_1',
+    link: 'https://www.soccerstats.com/results.asp?league=netherlands&pmtype=bydate',
+  },
+  {
+    id: 'norway_1',
+    link: 'https://www.soccerstats.com/results.asp?league=norway&pmtype=bydate',
+  },
+  {
+    id: 'portugal_1',
+    link: 'https://www.soccerstats.com/results.asp?league=portugal&pmtype=bydate',
+  },
+  {
+    id: 'sweden_1',
+    link: 'https://www.soccerstats.com/results.asp?league=sweden&pmtype=bydate',
+  },
+  {
+    id: 'switzerland_1',
+    link: 'https://www.soccerstats.com/results.asp?league=switzerland&pmtype=bydate',
+  },
+  {
+    id: 'spain_1',
+    link: 'https://www.soccerstats.com/results.asp?league=spain&pmtype=bydate',
+  },
+  {
+    id: 'turkey_1',
+    link: 'https://www.soccerstats.com/results.asp?league=turkey&pmtype=bydate',
+  },
+];
