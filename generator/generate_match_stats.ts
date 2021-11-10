0bd5001457094ad38b943b5215c0853e6950aa52
AkraichLand <54623031+AkraichLand@users.noreply.github.com>
2021-11-10 23:57:23 +0000

    Adding tests for generate_match_stats func

--- generator/generate_match_stats.ts
+++ generator/generate_match_stats.ts
@@ -14,8 +14,12 @@ const generate_match_stats = (
   home_2HG: number,
   away_2HG: number
 ): StatsType => {
-  let stats = { ...initialStats };
-  //console.log('generate_match_stats', stats);
+  let stats = {
+    ...initialStats,
+    _1HT: { ...initialStats._1HT },
+    _2HT: { ...initialStats._2HT },
+  };
+
   // Checking for GP, W, D, L, GF, GA, GD, Pts
   stats.GP += 1;
   stats.GF += home_FTG;
@@ -29,6 +33,43 @@ const generate_match_stats = (
   } else {
     stats.Pts += 1;
     stats.D += 1;
+  }
+
+  // Checking for FTS, BTS, CS, S2G, C2G, S3G, C3G
+  if (home_FTG === 0) {
+    stats.FTS += 1;
+  }
+  if (away_FTG === 0) {
+    stats.CS += 1;
+  }
+  if (home_FTG * away_FTG > 0) {
+    stats.BTS += 1;
+  }
+  if (home_FTG >= 2) {
+    stats.S2G += 1;
+  }
+  if (away_FTG >= 2) {
+    stats.C2G += 1;
+  }
+  if (home_FTG >= 3) {
+    stats.S3G += 1;
+  }
+  if (away_FTG >= 3) {
+    stats.C3G += 1;
+  }
+
+  // Checking for P15, P25, P35, P45
+  if (home_FTG + away_FTG >= 2) {
+    stats.P15 += 1;
+  }
+  if (home_FTG + away_FTG >= 3) {
+    stats.P25 += 1;
+  }
+  if (home_FTG + away_FTG >= 4) {
+    stats.P35 += 1;
+  }
+  if (home_FTG + away_FTG >= 5) {
+    stats.P45 += 1;
   }
 
   // Checking for W_W, W_D, W_L, D_W, D_D, D_L, L_W, L_D, L_L
@@ -57,6 +98,8 @@ const generate_match_stats = (
       stats.DD += 1;
     }
   }
+
+  console.log(stats._1HT);
 
   // Checking for _1H :   W, D, L, FTS, CS, BTS, S1G, S2G, C1G, C2G, P15,
   if (home_1HG > away_1HG) {
@@ -87,7 +130,7 @@ const generate_match_stats = (
   if (away_1HG >= 2) {
     stats._1HT.C2G += 1;
   }
-  if (home_1HG + away_1HG > 2) {
+  if (home_1HG + away_1HG >= 2) {
     stats._1HT.P15 += 1;
   }
 
@@ -115,47 +158,13 @@ const generate_match_stats = (
     stats._2HT.C1G += 1;
   }
   if (home_2HG >= 2) {
-    stats._2HT.S2G += 2;
+    stats._2HT.S2G += 1;
   }
   if (away_2HG >= 2) {
     stats._2HT.C2G += 1;
   }
-  if (home_2HG + away_2HG > 2) {
+  if (home_2HG + away_2HG >= 2) {
     stats._2HT.P15 += 1;
-  }
-
-  // Checking for FTS, CS, S2G, C2G, S3G, C3G
-  if (home_FTG === 0) {
-    stats.FTS += 1;
-  }
-  if (away_FTG === 0) {
-    stats.CS += 1;
-  }
-  if (home_FTG >= 2) {
-    stats.S2G += 1;
-  }
-  if (away_FTG >= 2) {
-    stats.C2G += 1;
-  }
-  if (home_FTG >= 3) {
-    stats.S3G += 1;
-  }
-  if (away_FTG >= 3) {
-    stats.C3G += 1;
-  }
-
-  // Checking for P15, P25, P35, P45
-  if (home_FTG + away_FTG >= 2) {
-    stats.P15 += 1;
-  }
-  if (home_FTG + away_FTG >= 3) {
-    stats.P25 += 1;
-  }
-  if (home_FTG + away_FTG >= 4) {
-    stats.P35 += 1;
-  }
-  if (home_FTG + away_FTG >= 5) {
-    stats.P45 += 1;
   }
 
   return stats;
